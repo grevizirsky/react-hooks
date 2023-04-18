0a7ae3ff588207bfab1eb4af806f00a05bca8feb
Adriano Grevizirsky <grevizirsky@gmail.com>
2023-04-18 18:20:34 -0300

    Hook - useReducer

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import HooksUseState from '../components/HooksUseState'
+import HookUseReducer from '../components/HookUseReducer'
 
 const Home = () => {
   return (
     <div>
         <HooksUseState />
+        <HookUseReducer />
     </div>
   )
 }
