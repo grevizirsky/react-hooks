001241b2a0078022618236e1ba30525e3600ef65
Adriano Grevizirsky <grevizirsky@gmail.com>
2023-04-21 18:00:00 -0300

    Hook - useContext

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,12 +4,21 @@ import HooksUseState from '../components/HooksUseState'
 import HookUseReducer from '../components/HookUseReducer'
 import HookUseEffect from '../components/HookUseEffect'
 
+//useContext
+import { useContext } from 'react'
+import { SomeContext } from '../components/HookUseContext'
+
 const Home = () => {
+  const { contextValue } = useContext(SomeContext)
+
   return (
     <div>
         <HooksUseState />
         <HookUseReducer />
         <HookUseEffect />
+        <h2>useContext</h2>
+        <p>Valor do contexto: {contextValue}</p>
+        <hr />
     </div>
   )
 }
